a087b8de0f2038a9d1afc1379ea5208afe256279
luccui000 <luccui2k@gmail.com>
2022-07-30 20:31:52 +0700

    initialize project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lực Cui Theme</title>
+</head>
+<body>
+    Hello world
+</body>
+</html>
